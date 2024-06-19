0fc671fca871b9408545774465ea6a72830122dd
INSTRUCTOR <INSTRUCTOR@PERAPRCSAFSD492.FormacionSena.RED>
2024-06-19 15:24:17 -0500

    vuelta prueba registrar de bd

--- src/componentes/Registro.js
+++ src/componentes/Registro.js
@@ -127,16 +127,8 @@ export default function Registro() {
         //     headers: { "Content-Type": "application/json", 'Accept': 'application/json' },
         //     body: JSON.stringify(values)
         // })
-            // console.log("URL---->",URL)
-            // fetch(`${URL}/registro-usuario` , {
-            //     method: "POST",
-            //     headers: {
-            //         "Content-Type": "application/json", "Accept": "application/json"},
-            //     body:JSON.stringify(values)
-            // })
-
             console.log("URL---->",URL)
-            fetch(`${URL}/sql5714458` , {
+            fetch(`${URL}/registro-usuario` , {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json", "Accept": "application/json"},
