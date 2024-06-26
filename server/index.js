5d7337a17efab7f565f3dc1ad33fdbb0d572c610
INSTRUCTOR <INSTRUCTOR@PERAPRCSAFSD492.FormacionSena.RED>
2024-06-26 15:56:27 -0500

    nueva base de datos 3

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json())
 app.use(cors())
 
 //Solicitamos la conexión a la BD
-const conexion = require('./configDB/configDB.js')
+const conexion = require('../configDB.js')
 
 app.get("/todos-los-Usuarios", (req, res) => {
 conexion.connect(function (err) {
